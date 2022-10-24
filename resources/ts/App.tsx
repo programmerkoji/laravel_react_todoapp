1d15c9e23632b16293ec32212b50bc1780c7a51e
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2022-10-24 22:01:59 +0900

    ログイン機能の実装

--- resources/ts/App.tsx
+++ resources/ts/App.tsx
@@ -1,27 +1,30 @@
 import React from "react";
-import Router from './router/router'
-import { QueryClient, QueryClientProvider } from "react-query"
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+import Router from "./router/router";
+import { AuthProvider } from "./hooks/AuthContext";
 
 const App: React.VFC = () => {
     const queryClient = new QueryClient({
         defaultOptions: {
             queries: {
-                retry: false //エラー時に何回再取得を行うか（falseは行わない）
+                retry: false, //エラー時に何回再取得を行うか（falseは行わない）
             },
             mutations: {
-                retry: false
-            }
-        }
-    })
+                retry: false,
+            },
+        },
+    });
     return (
-        <QueryClientProvider client={queryClient}>
-            <Router />
-            <ToastContainer hideProgressBar={true} />
-        </QueryClientProvider>
-    )
+        <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <Router />
+                <ToastContainer hideProgressBar={true} />
+            </QueryClientProvider>
+        </AuthProvider>
+    );
 };
 
 export default App;
---
+++ resources/ts/pagaes/error/index.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const NotFoundPage: React.VFC = () => {
+    return (
+        <div className="align-center">
+            <h1>404 Not Found</h1>
+            <p>お探しのページは見つかりませんでした。</p>
+        </div>
+    );
+};
+
+export default NotFoundPage;
